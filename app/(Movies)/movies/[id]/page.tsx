31ed5675584afd19decc05a2afd2bb0e0ab1937a
noname <cloudeven@gmail.com>
2024-02-27 11:32:30 +0900

    .

--- app/(Movies)/movies/[id]/page.tsx
+++ app/(Movies)/movies/[id]/page.tsx
@@ -1,10 +1,9 @@
-import { Metadata } from 'next'
 import { Suspense } from 'react'
 import MovieInfo, { getMovie } from '../../../../components/movie-info'
 import MovieVideos from '../../../../components/movie-videos'
-import MovieProviders from '../../../../components/movie-providers'
-import MovieSimilar from '../../../../components/movie-similar'
-import MovieCredits from '../../../../components/movie-credits'
+// import MovieProviders from '../../../../components/movie-providers'
+// import MovieSimilar from '../../../../components/movie-similar'
+// import MovieCredits from '../../../../components/movie-credits'
 
 interface IParams { params: {id: string} }
 
@@ -34,36 +33,5 @@ export default async function MovieDetail({params: {id}}: IParams) {
         </div>
     )
 }
-
-
-// import { Metadata } from "next"
-// import { API_MOVIE_LIST_URL } from "../../../(Home)/page"
-
-// export const metadata: Metadata = {
-//     title: 'Movie'
-// }
-
-// async function getMovie(id: string) {
-//     const response = await fetch(`${API_MOVIE_LIST_URL}/${id}`)
-//     const json = await response.json()
-//     return json
-// }
-
-// async function getVideo(id: string) {
-//     const response = await fetch(`${API_MOVIE_LIST_URL}/${id}/videos`)
-//     const json = await response.json()
-//     return json
-// }
-
-// export default async function MovieDetailPromise({params: {id}}: {params: {id: string}}) {
-//     const [movie, videos] = await Promise.all([getMovie(id), getVideo(id)])
-//     return (
-//         <div>
-//             <h1>{movie.title}</h1>
-//             <h6>{JSON.stringify(videos)}</h6>
-//         </div>
-//     )
-// }
-
 
 // test url: http://localhost:3000/movies/123?region=kr&page=2
